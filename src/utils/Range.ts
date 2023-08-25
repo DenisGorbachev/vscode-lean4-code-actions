04089d446a804d72d7d38a14cddfb7088085f75a
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-08-25 14:22:00 +0700

    Code style fix

--- src/utils/Range.ts
+++ src/utils/Range.ts
@@ -1,4 +1,4 @@
-import { Position, Range } from "vscode"
+import { Position, Range } from 'vscode'
 
 export const getRangeFromOffsets = (positionAt: (offset: number) => Position) => (offsetLeft: number, offsetRight: number) => {
   return new Range(positionAt(offsetLeft), positionAt(offsetRight))
