03c425bb75c75057a19ce40fc930cdca8fb7ca00
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-10 15:55:51 +0700

    aider: Added /Users/starfall/workspace/vscode-lean4-code-actions/src/extension.ts

--- src/commands/renameDeclaration.ts
+++ src/commands/renameDeclaration.ts
@@ -3,7 +3,7 @@ import { merge } from 'remeda'
 import { getFileInfoFromEditor, getNewUri } from 'src/models/FileInfo'
 import { NewTypeKeywordSchema } from 'src/models/NewTypeKeyword'
 import { withWorkspaceEdit } from 'src/utils/WorkspaceEdit'
-import { Uri, WorkspaceEdit, window } from 'vscode'
+import { Range, TextEditor, WorkspaceEdit, window } from 'vscode'
 import { z } from 'zod'
 import { ensureEditor } from '../utils/TextEditor'
 
@@ -19,8 +19,8 @@ export async function renameDeclaration() {
   const newInfo = merge(oldInfo, { name: newName })
   const newUri = getNewUri(editor.document.uri, newInfo)
   return withWorkspaceEdit(async edit => {
-    replaceDeclarationName(oldName, newName)(oldUri)(edit)
-    replaceNamespace(oldName, newName)(oldUri)(edit)
+    replaceDeclarationName(oldName, newName)(editor)(edit)
+    replaceNamespace(oldName, newName)(editor)(edit)
     edit.renameFile(oldUri, newUri)
   })
 }
@@ -35,30 +35,29 @@ const askName = async (oldName: string) => {
   }))
 }
 
-const replaceDeclarationName = (oldName: string, newName: string) => (uri: Uri) => (edit: WorkspaceEdit) => {
+const replaceDeclarationName = (oldName: string, newName: string) => (editor: TextEditor) => (edit: WorkspaceEdit) => {
   return NewTypeKeywordSchema.options.map(keyword => {
     const prev = `${keyword} ${oldName}`
     const next = `${keyword} ${newName}`
-    findReplace(prev, next)(uri)(edit)
+    findReplace(prev, next)(editor)(edit)
   })
 }
 
-const replaceNamespace = (oldName: string, newName: string) => (uri: Uri) => (edit: WorkspaceEdit) => {
+const replaceNamespace = (oldName: string, newName: string) => (editor: TextEditor) => (edit: WorkspaceEdit) => {
   const prev = `namespace ${oldName}`
   const next = `namespace ${newName}`
-  findReplace(prev, next)(uri)(edit)
+  findReplace(prev, next)(editor)(edit)
 }
 
-import { workspace, Range, Position } from 'vscode'
-
-const findReplace = (prev: string, next: string) => (uri: Uri) => async (edit: WorkspaceEdit) => {
-  const document = await workspace.openTextDocument(uri)
-  const text = document.getText()
+const findReplace = (prev: string, next: string) => (editor: TextEditor) => (edit: WorkspaceEdit) => {
+  const { document } = editor
+  const { uri, getText, positionAt } = document
+  const text = getText()
   let startIndex = text.indexOf(prev)
   while (startIndex !== -1) {
     const endIndex = startIndex + prev.length
-    const start = document.positionAt(startIndex)
-    const end = document.positionAt(endIndex)
+    const start = positionAt(startIndex)
+    const end = positionAt(endIndex)
     const range = new Range(start, end)
     edit.replace(uri, range, next)
     startIndex = text.indexOf(prev, endIndex)
